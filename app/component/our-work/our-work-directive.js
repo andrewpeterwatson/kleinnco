2d1879e1b1dc214adf52c0c805e2a491d493e601
Andrew Watson <andrewwatson@Andrew-Watsons-Mac-Pro.local>
2016-10-20 12:26:58 -0700

    added data and service dir as well as made gallery cycle

--- app/component/our-work/our-work-directive.js
+++ app/component/our-work/our-work-directive.js
@@ -5,8 +5,49 @@ angular.module('kleinnco')
 .directive('ourWork', function(){
   return {
     restrict: 'E',
-    replace: true,
     template: require('./our-work.html'),
+    controller: ['galleryService', OurWorkController],
+    controllerAs: 'ourWorkCtrl',
     scope: {}
   };
+
+function OurWorkController(galleryService){
+
+  const clients = require('json!../../data/our-work-data.json')
+  this.currentGalleryIdx = 2;
+
+  this.currentClientName = clients;
+  this.currentClientWork = clients;
+
+  this.cycleGalleryBack = function() {
+    galleryService.cycleBackward(this.currentGalleryIdx);
+    this.currentGalleryIdx = galleryService.galleryIndex
+  }
+
+  this.cycleGallerForward = function() {
+    galleryService.cycleForward(this.currentGalleryIdx, clients.length);
+    this.currentGalleryIdx = galleryService.galleryIndex;
+  }
+
+
+}
 });
+
+
+
+
+// function BrewGalleryController(brewMethodGalleryService, userSelectionsService){
+//
+//     this.brewMethods = require('json!../../assets/brew-method-data/brew-methods.json');
+//     this.galleryIndex = 0;
+//     this.currentBrewMethod = userSelectionsService.userSelections.methodId;
+//
+//     this.stepForward = function() {
+//       brewMethodGalleryService.cycleForward(this.galleryIndex, this.brewMethods[userSelectionsService.userSelections.methodId].length);
+//       this.galleryIndex = brewMethodGalleryService.galleryIndex;
+//     };
+//     this.stepBackward = function(){
+//       brewMethodGalleryService.cycleBackward(this.galleryIndex);
+//       this.galleryIndex = brewMethodGalleryService.galleryIndex;
+//     };
+//   }
