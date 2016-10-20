c2e6d4e155fec4cf2405ce38929fd42843cb6401
Andrew Watson <andrewpeterwatson@gmail.com>
2016-10-19 19:34:21 -0700

    added features to scroll

--- app/component/intro/intro-directive.js
+++ app/component/intro/intro-directive.js
@@ -7,6 +7,27 @@ angular.module('kleinnco')
     restrict: 'E',
     replace: true,
     template: require('./intro.html'),
+    controller: IntroController,
+    controllerAs: 'introCtrl',
     scope: {}
   };
+
+  function IntroController(){
+
+    $(function() {
+      var controller = new ScrollMagic.Controller();
+      var tween = TweenMax.to('.intro-image', 0.5, {
+        scale: (0.5)
+      });
+      var containerScene = new ScrollMagic.Scene({
+        triggerElement: '.image-container',
+        duration: 500
+      })
+    .setPin('.image-block')
+    .setTween(tween)
+    .addIndicators()
+    .addTo(controller);
+    });
+
+  }
 });
