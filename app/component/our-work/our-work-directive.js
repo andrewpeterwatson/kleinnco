fa1d12f3fb2defec666c6c110c07db2bc3c3118c
Andrew Watson <andrewpeterwatson@gmail.com>
2016-10-21 10:10:43 -0700

    began media queries

--- app/component/our-work/our-work-directive.js
+++ app/component/our-work/our-work-directive.js
@@ -11,35 +11,35 @@ angular.module('kleinnco')
     scope: {}
   };
 
-function OurWorkController(galleryService){
+  function OurWorkController(galleryService){
 
-  const clients = require('json!../../data/our-work-data.json')
-  this.currentGalleryIdx = 0;
-  this.currentClient = clients;
-  this.Microsoft = true;
-  this.Amazon = false
-  this.Netflix = false
+    const clients = require('json!../../data/our-work-data.json');
+    this.currentGalleryIdx = 0;
+    this.currentClient = clients;
+    this.Microsoft = true;
+    this.Amazon = false;
+    this.Netflix = false;
 
 
-  this.cycleGalleryBack = function() {
-    this[this.currentClient[this.currentGalleryIdx].name] = false;
-    galleryService.cycleBackward(this.currentGalleryIdx);
-    this.currentGalleryIdx = galleryService.galleryIndex
-    this[this.currentClient[this.currentGalleryIdx].name] = true;
+    this.cycleGalleryBack = function() {
+      this[this.currentClient[this.currentGalleryIdx].name] = false;
+      galleryService.cycleBackward(this.currentGalleryIdx);
+      this.currentGalleryIdx = galleryService.galleryIndex;
+      this[this.currentClient[this.currentGalleryIdx].name] = true;
+    };
+
+    this.cycleGallerForward = function() {
+      this[this.currentClient[this.currentGalleryIdx].name] = false;
+      galleryService.cycleForward(this.currentGalleryIdx, clients.length);
+      this.currentGalleryIdx = galleryService.galleryIndex;
+      this[this.currentClient[this.currentGalleryIdx].name] = true;
+    };
+
+    this.testToggle = function(){
+      this.currentPosition = !this.currentPosition;
+    };
+
+
+
   }
-
-  this.cycleGallerForward = function() {
-    this[this.currentClient[this.currentGalleryIdx].name] = false;
-    galleryService.cycleForward(this.currentGalleryIdx, clients.length);
-    this.currentGalleryIdx = galleryService.galleryIndex;
-    this[this.currentClient[this.currentGalleryIdx].name] = true
-  }
-
-this.testToggle = function(){
-  this.currentPosition = !this.currentPosition;
-}
-
-
-
-}
 });
