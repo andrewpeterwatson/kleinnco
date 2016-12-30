68ffeb71f5e794eb248f785b34a3c91dc97b202d
Andrew Watson <andrewwatson@Andrew-Watsons-Mac-Pro.local>
2016-12-29 20:49:33 -0800

    added changes to intro

--- app/component/about-page/about-page-directive.js
+++ app/component/about-page/about-page-directive.js
@@ -4,8 +4,16 @@ const angular = require('angular');
 angular.module('kleinnco')
 .directive('aboutPage', function(){
   return {
-    restrict: 'E',
     template: require('./about-page.html'),
     scope: {}
+    controller: ['$location', AboutPageController],
+    controllerAs: 'aboutCtrl',
+    bindToController: true
   };
 });
+
+function AboutPageController($location) {
+  this.goHome = function() {
+    $location.path('/home')
+  }
+}
