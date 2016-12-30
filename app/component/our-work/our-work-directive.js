02d08fcb184bc59079a56fb947cec74247468a3a
Andrew Watson <andrewwatson@Andrew-Watsons-Mac-Pro.local>
2016-12-30 09:09:59 -0800

    testing

--- app/component/our-work/our-work-directive.js
+++ app/component/our-work/our-work-directive.js
@@ -19,7 +19,7 @@ angular.module('kleinnco')
 
     this.currentIndex = 0;
     this.showWorkGallery = false;
-    this.testImg = 'http://images.askmen.com/1080x540/2015/12/04-031429-instagram_filter_baby_names_are_trending_hard.jpg'
+    this.testImg = 'img/eab6d74b1a669538cc70a5375e302eb1.png'
 
 
 
